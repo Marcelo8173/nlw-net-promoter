8552acdd7ad324d4fee1f7925eb1d64758951ea7
Marcelo8173 <marceloandrebio@gmail.com>
2021-02-23 10:45:19 -0300

    route create user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Router } from 'express';
+import {UserController} from '../controller/userController';
 
 const routes = Router();
+const userController = new UserController();
 
 routes.get('/users', (resquest: Request, response: Response) => {
     return response.json({msg:'olá'});
 })
 
-routes.post('')
+routes.post('/users', userController.create);
 
 export default routes;
