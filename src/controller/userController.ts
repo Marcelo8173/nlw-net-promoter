70c96accc56b0e0621ef0bec67f2681cbc423f66
Marcelo8173 <marceloandrebio@gmail.com>
2021-02-24 09:23:29 -0300

    email exist validation

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -7,6 +7,16 @@ class UserController{
         const {name,email} = request.body;
         const userRepository = getRepository(User);
 
+        const alredyEsxist = await userRepository.findOne({
+            where: email
+        })
+
+        if (alredyEsxist) {
+            return response.status(400).json({
+                error: 'user alredy exist'
+            });
+        };
+        
         const user = userRepository.create({
             name,
             email
